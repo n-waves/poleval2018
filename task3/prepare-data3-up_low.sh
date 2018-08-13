ccec748a9afe333e9fc5ccdbcee5287babfb91e8
Piotr Czapla <github@piotrczapla.com>
2018-08-13 23:47:57 +0200

    Move character_coverage to the spm_train

--- task3/prepare-data3-up_low.sh
+++ task3/prepare-data3-up_low.sh
@@ -28,7 +28,7 @@ if [ ! -f "${SENTENCEPIECE_MODEL_NAME}.model" ]; then
     n|N ) echo "Exiting";exit 1;;
     * ) echo "Invalid answer";exit 1;;
   esac
-  spm_train --input="${UNIQ_SENTENCE_FILE}" --model_prefix="${SENTENCEPIECE_MODEL_NAME}" --vocab_size=100000 --unk_id=0 --pad_id=1 --bos_id=2 --eos_id=3 --input_sentence_size=30000000 --model_type=unigram
+  spm_train --input="${UNIQ_SENTENCE_FILE}" --character_coverage 1.0 --model_prefix="${SENTENCEPIECE_MODEL_NAME}" --vocab_size=100000 --unk_id=0 --pad_id=1 --bos_id=2 --eos_id=3 --input_sentence_size=30000000 --model_type=unigram
 else
   echo "Setencepiece model '${SENTENCEPIECE_MODEL_NAME}.model' already exists."
 fi
@@ -43,7 +43,7 @@ if [ ! -f ${IDS_FILE} ]; then
     rm -fr "${PARTS_DIR}"/sentence_part-*
     split -n "l/${SPM_PROCESSES}" "${TEMP_DIR}/escaped.txt" "${PARTS_DIR}/sentence_part-"
 
-    ls "${PARTS_DIR}"/sentence_part-* | xargs '-I{}' -P "${SPM_PROCESSES}" -n 1 spm_encode --character_coverage 1.0 --model="${SENTENCEPIECE_MODEL_NAME}.model" --extra_options=bos:eos --output_format=id '--output={}.ids' '{}'
+    ls "${PARTS_DIR}"/sentence_part-* | xargs '-I{}' -P "${SPM_PROCESSES}" -n 1 spm_encode  --model="${SENTENCEPIECE_MODEL_NAME}.model" --extra_options=bos:eos --output_format=id '--output={}.ids' '{}'
     cat "${PARTS_DIR}"/sentence_part-*.ids > "${IDS_FILE}"
 fi
 if [ ! -f "${OUTPUT_DIR}/val_ids.npy" ] || [ ! -f "${OUTPUT_DIR}/trn_ids.npy" ]; then
